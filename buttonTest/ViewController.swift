f79a3e67f6210e0a1fdf6782e022c6c240148567
piremi <piremi@gmail.com>
2017-09-14 19:54:48 +0900

    first stage for basic learning

--- buttonTest/ViewController.swift
+++ buttonTest/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func tabBtn() {
+        myLabel.text = "안녕하세요!"
+    }
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
